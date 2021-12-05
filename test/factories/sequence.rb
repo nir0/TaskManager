c11acf5681aec08c35f6c178dfca88891b3e90b2
Yuriy Shuin <yuriy5sh@gmail.com>
2021-12-05 15:19:33 +0300

    refactoring_test

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,9 +1,41 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description, :state, :expired_at] do |n|
-    "string#{n}"
-  end
+  # sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description, :state, :expired_at] do |n|
+  #   "string#{n}"
+  # end
 
   sequence :email do |n|
     "person#{n}@test.com"
+  end
+
+  sequence :first_name do |n|
+    "first_name#{n}@test.com"
+  end
+
+  sequence :last_name do |n|
+    "last_name#{n}@test.com"
+  end
+
+  sequence :password do |n|
+    "password#{n}@test.com"
+  end
+
+  sequence :avatar do |n|
+    "avatar#{n}@test.com"
+  end
+
+  sequence :name do |n|
+    "name#{n}@test.com"
+  end
+
+  sequence :description do |n|
+    "description#{n}@test.com"
+  end
+
+  sequence :expired_at do |n|
+    "expired_at#{n}@test.com"
+  end
+
+  sequence :string do |n|
+    "password#{n}@test.com"
   end
 end
