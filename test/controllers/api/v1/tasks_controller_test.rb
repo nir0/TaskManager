add6dd1721b0cb9baeb30e10d119541008718e90
Yuriy Shuin <yuriy5sh@gmail.com>
2021-12-12 16:29:58 +0300

    rubocop

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 class Api::V1::TasksControllerTest < ActionController::TestCase
-
   test 'should get show' do
-    author = create :user
-    task = create :task, author: author
+    author = create(:user)
+    task = create(:task, author: author)
     get :show, params: { id: task.id, format: :json }
     assert_response :success
   end
@@ -15,11 +14,11 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
   end
 
   test 'should post create' do
-    author = create :user
+    author = create(:user)
     sign_in(author)
-    assignee = create :user
-    task_attributes = attributes_for(:task)
-      .merge({ assignee_id: assignee.id })
+    assignee = create(:user)
+    task_attributes = attributes_for(:task).
+      merge({ assignee_id: assignee.id })
     post :create, params: { task: task_attributes, format: :json }
     assert_response :created
 
@@ -31,12 +30,12 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
   end
 
   test 'should put update' do
-    author = create :user
-    assignee = create :user
-    task = create :task, author: author
-    task_attributes = attributes_for(:task)
-      .merge({ author_id: author.id, assignee_id: assignee.id })
-      .stringify_keys
+    author = create(:user)
+    assignee = create(:user)
+    task = create(:task, author: author)
+    task_attributes = attributes_for(:task).
+      merge({ author_id: author.id, assignee_id: assignee.id }).
+      stringify_keys
 
     patch :update, params: { id: task.id, format: :json, task: task_attributes }
     assert_response :success
@@ -46,12 +45,11 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
   end
 
   test 'should delete destroy' do
-    author = create :user
-    task = create :task, author: author
+    author = create(:user)
+    task = create(:task, author: author)
     delete :destroy, params: { id: task.id, format: :json }
     assert_response :success
 
     assert !Task.where(id: task.id).exists?
   end
-
 end
