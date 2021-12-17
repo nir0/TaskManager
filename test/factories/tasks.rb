9f1ec638b012dfb4ca16ec9945e0746bea888e82
Yuriy Shuin <yuriy5sh@gmail.com>
2021-12-17 18:15:26 +0300

    edit factories_tasks

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,10 @@ FactoryBot.define do
   factory :task do
     name
     description
-    association :author, factory: :manager
-    association :assignee, factory: :developer
-    expired_at
+    # association :author, factory: :manager
+    # association :assignee, factory: :developer
+    # expired_at
+    author_id { create :user }
+    assignee_id { create :user }
   end
 end
